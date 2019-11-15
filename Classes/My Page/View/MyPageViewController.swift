2103cfec2d512906e9c9da1885c5db7649965d22
florianldt <florian.ludot@gmail.com>
2019-11-15 20:36:01 +0200

    init project

---
+++ Classes/My Page/View/MyPageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageViewController.swift
+//  Trivi
+//
+//  Created by Florian LUDOT on 11/13/19.
+//  Copyright © 2019 Florian Ludot. All rights reserved.
+//
+
+import UIKit
+
+class MyPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.Names.white.color
+    }
+
+}
